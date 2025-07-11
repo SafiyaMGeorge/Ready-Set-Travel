9869a03bd1c9227a1b54868856a53ee0919ce6ee
Safiya-Yourbookteam-website-intern <safiyayourbookteam@gmail.com>
2025-07-11 17:41:24 -0400

    Toggle menu function

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function (){
+  $('#menuBtn').click(function () {
+    $('#collapsedNav').toggle()
+  })
+});
+
+let booking = document.getElementsByClassName("book");
+console.log(booking);
